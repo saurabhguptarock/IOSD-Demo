ca56346df03f9d8119717d8b35a8b1a10a23ab51
saurabh gupta <saurabhguptarock119@gmail.com>
2020-11-08 16:34:38 +0530

    fixed some bugs

--- lib/pages/topics.dart
+++ lib/pages/topics.dart
@@ -10,15 +10,15 @@ class TopicsPage extends StatefulWidget {
 
 class _TopicsPageState extends State<TopicsPage> {
   static List<String> _images = [
-    'assets/images/1.jpg'
-        'assets/images/2.jpg'
-        'assets/images/3.jpg'
-        'assets/images/4.jpg'
-        'assets/images/5.jpg'
-        'assets/images/6.jpg'
-        'assets/images/7.jpg'
-        'assets/images/8.jpg'
-        'assets/images/9.jpg'
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
+    'assets/images/7.jpg',
+    'assets/images/8.jpg',
+    'assets/images/9.jpg',
   ];
 
   @override
